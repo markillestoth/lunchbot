3b59856098e4d1f0a978338bc1fcaacc4e27c241
markillestoth <34168046+markillestoth@users.noreply.github.com>
2019-04-23 13:37:42 +0200

    Update index.js
    test

--- index.js
+++ index.js
@@ -43,42 +43,11 @@ var params = {
 //  Sources
 //
 
-// La Tabla
-const latabla = new FacebookSource('es', 'La Tabla', "827767180609816", params);
+// Seasons
+const seasons = new FacebookSource('fallen_leaf', 'Seasons', "222194971977", params);
 
-// KPK
-const kpk = new FacebookSource('scissors', 'Kivi Paber Käärid', 'kivipaberkaarid', params);
 
-// Apelsini Raudtee
-const apelsin = new FacebookSource('tangerine', 'Apelsini Raudtee', 'apelsiniraudtee', params);
-
-// F-Hoone
-const fhoone = new FacebookSource('house', 'F-Hoone', 'Fhoone', params);
-
-// Kukeke, occassionally has an offer posted
-const kukeke = new FacebookSource('rooster', 'Kukeke', 'kukekene', params);
-
-// Trühvel, special because only posts once a week (on Mondays)
-const truhvel = new FacebookSource('coffee', 'Trühvel', '1829502837275034', params);
-
-// Kohvik Sõbrad
-const buddies = new FacebookSource('two_men_holding_hands', 'Kohvik Sõbrad', 'kohviksobrad', params);
-
-// Frenchy
-const frenchy = new FacebookSource('fr', 'Frenchy', '593232130762873', params);
-
-// Sesoon
-const sesoon = new FacebookSource('eggplant', 'Kohvik Sesoon', 'KohvikSesoon', params);
-
-const luncherServices = [
-    ['steam_locomotive', 'Perrooni Kohvik', '5795c85ec7e7ff48390001eb'],
-    ['tea', 'Telliskivi Reval Café', '575ea00fc7e7ff483900004b'],
-    ['poultry_leg', 'Telliskivi 15', '5761148ec7e7ff4839000081']
-].map(function(service) {
-    return luncherSource.apply(null, service);
-});
-
-const services = [latabla, kpk, apelsin, fhoone, truhvel, kukeke, buddies, frenchy, sesoon].concat(luncherServices);
+const services = [seasons];
 console.log('Starting LunchBot with ' + services.length + ' services');
 
 bot.services = services;
